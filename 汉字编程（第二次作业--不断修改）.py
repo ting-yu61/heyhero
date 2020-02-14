8004f37cfeb9ba17a89ddc4c9ceb343058a25756
ting-yu61 <1091222140@qq.com>
2020-02-14 14:56:13 +0800

    提交文件

--- 汉字编程（第二次作业--不断修改）.py
+++ 汉字编程（第二次作业--不断修改）.py
@@ -6,54 +6,104 @@
 import re
 tonum= {'零': 0, '一':1, '二':2, '三':3, '四':4, '五':5, '六':6, '七':7, '八':8, '九':9, '十':10} 
 tochinese= {0:'零', 1:'一', 2:'二',3:'三', 4:'四', 5:'五', 6:'六', 7:'七', 8:'八', 9:'九', 10:'十'}
-def hantoshuzi(x):  #汉字转为数字
-    if x[0]=='负':
-        if len(x)==2:                 #例：负一
-            a=(-1)*tonum[x[1]]
-        if len(x)==3:   
-            if x[1:]=='一百':         #负一百
-                a=-100
-            if x[1]=='十':            #例：负十二
-                a=(-1)*tonum[x[2]]-10
-            if x[2]=='十':            #例：负六十
-                a=(-10)*tonum[x[1]]
-        if len(x)==4:                 #例：负六十二
-            a=(-10)*tonum[x[1]]-tonum[x[3]]
+#判断数字格式
+def num(s):
+    pd=['负','零','一','二','三','四','五','六','七','八','九','十']
+    if s=='一百' or s=='负一百':
+        x=1
     else:
-        if len(x)==1:                 #例：一
-            a=tonum[x]
-        if len(x)!=1:
-            if len(x)==2:
-                if x=='一百':         #一百
-                    a=100
-                elif x[0]=='十':     #例：十二
-                    a=10+tonum[x[1]]
-                elif x[1]=='十':     #例：六十
-                    a=tonum[x[0]]*10
-            elif len(x)==3:          #例：六十二
-                a=tonum[x[0]]*10+tonum[x[2]]
-    return a  
-def shuzitohan(x):  #数字转为汉字
+        for i in s:
+            if i in pd:
+                x=1
+            else:
+                x=0
+                break
+    if x==1:
+        ss=s
+        if s[0]=='负':
+            ss=s[1:]
+        if  re.match('^\w$',ss)!=None or re.match('^十\w$',ss)!=None or re.match('^\w十$',ss)!=None or re.match('^\w十\w$',ss)!=None or ss=='一百':
+            str='True'
+        else:
+            str='False'
+    else:
+        str='False'
+    return str
+#汉字转为数字
+def hantoshuzi(s):
+    if s[0]=='负':
+        if len(s)==2 and s[1]!='零': #例：负()  【排除：负零】
+            x=(-1)*tonum[s[1]]
+        elif len(s)==3:   
+            if s[1:]=='一百':         #负一百
+                x=-100
+            elif s[1]=='十' and s[2]!='十':       #例：负十() 【排除：负十十】
+                x=(-1)*tonum[s[2]]-10
+            elif s[1]!='十' and s[1]!='一' and s[2]=='十':   #例：负()十 【排除：负十十，负一十】
+                x=(-10)*tonum[s[1]]
+            else:
+                x='False'
+        elif len(s)==4 and s[3]!='十' and s[1]!='十' and s[0]!='一': 
+            x=(-10)*tonum[s[1]]-tonum[s[3]]      #例：负()十() 【排除：负六十十，负十十六，负一十六】
+        else:
+            x='False'
+    else:
+        if len(s)==1:                 #例：()
+            x=tonum[s]
+        if len(s)!=1:
+            if len(s)==2:
+                if s=='一百':         #一百
+                    x=100
+                elif s[0]=='十' and s[1]!='十':     #例：十()
+                    x=10+tonum[s[1]]
+                elif s[0]!='十' and s[0]!='一' and s[1]=='十':     #例：()十  【排除：十十，一十】
+                    x=tonum[s[0]]*10
+                else:
+                    x='False'
+            elif len(s)==3 and s[2]!='十' and s[0]!='十' and s[0]!='一': #例：()十() 【排除：六十十，十十六，一十六】
+                x=tonum[s[0]]*10+tonum[s[2]]
+            else:
+                x='False'
+    return x
+#数字转为汉字
+def shuzitohan(x):  
     if x>100 or x<-100:
         s='("'+str(x)+'",未能判断该数字!)'
     elif x<0 and x>=-100:
-        x*=(-1)
-    elif x<=10:           #例：1 —> 一
-        s=tochinese[x]
-    elif x==100:
-        s='一百'
-    elif x>10 & x<100:
-        a=x//10
-        b=x%10
-        if a==1:        #例：12 —> 十二
-            s='十'+tochinese[b]
-        elif a>1 & a<=9:    
-            if b!=0:
-                s=tochinese[a]+'十'+tochinese[b]   #例：62 —> 六十二
-            else:
-                s=tochinese[a]+'十'                #例：60 —> 六十
+        if x==-100:
+            s='负一百'
+        elif x<-10:
+            x*=(-1)
+            a=x//10
+            b=x%10
+            if a==1:        #例：12 —> 十二
+                s='负十'+tochinese[b]
+            elif a>1 & a<=9:    
+                if b!=0:
+                    s='负'+tochinese[a]+'十'+tochinese[b]   #例：62 —> 六十二
+                else:
+                    s='负'+tochinese[a]+'十'   
+        else:
+            x*=(-1)
+            s='负'+tochinese[x]
+    else:
+        if x<=10:           #例：1 —> 一
+            s=tochinese[x]
+        elif x==100:
+            s='一百'
+        elif x>10 & x<100:
+            a=x//10
+            b=x%10
+            if a==1:        #例：12 —> 十二
+                s='十'+tochinese[b]
+            elif a>1 & a<=9:    
+                if b!=0:
+                    s=tochinese[a]+'十'+tochinese[b]   #例：62 —> 六十二
+                else:
+                    s=tochinese[a]+'十'                #例：60 —> 六十
     return s
-def yunsuan(s,x,y):    #判断运算
+#判断运算
+def yunsuan(s,x,y):    
     if s=='增加':
         x+=y
     elif s=='减少':
@@ -64,26 +114,11 @@ def yunsuan(s,x,y):    #判断运算
         if y!=0:
             x=x//y
     return x
-def bijiao(s,x,y):    #判断比较
-    z=0
-    if (s=='大于' and x>y) or (s=='小于' and x<y) or (s=='等于' and x==y) or (s=='不等于' and x!=y) or (s=='大于等于' and x>=y) or (s=='小于等于' and x<=y):
-        z=1
-    return z
+#分割语句
 def fenge(s):
     yuju=s.split(' ')
     return yuju
-def pand(s):
-    a=['负','零','一','二','三','四','五','六','七','八','九','十']
-    if s=='一百' or s=='负一百':
-        str='True'
-    else:
-        for i in s:
-            if i in a:
-                str='True'
-            else:
-                str='False'
-                break
-    return str
+
 def main():
     an={}
     k=1
@@ -94,10 +129,10 @@ def main():
             if s=='退出':
                 break
             else:
-                print('(语句输入格式错误！)')
+                print('(语句输入格式错误！)') 
         else:
             if len(fenge(s))==4 and fenge(s)[0]=='整数' and fenge(s)[2]=='等于': #整数 （变量名） 等于 （数字）
-                    if pand(fenge(s)[3])=='True':
+                    if num(fenge(s)[3])=='True' and hantoshuzi(fenge(s)[3])!='False':
                         an[fenge(s)[1]]=hantoshuzi(fenge(s)[3])
                     else:
                         print('("%s",未能判断该数字!)' %fenge(s)[3])
@@ -105,24 +140,24 @@ def main():
                 if an.__contains__(fenge(s)[0])==True:
                     ss=fenge(s)[1]
                     x=an[fenge(s)[0]]
-                    if pand(fenge(s)[2])=='True':
+                    if num(fenge(s)[2])=='True' and hantoshuzi(fenge(s)[2])!='False':
                         y=hantoshuzi(fenge(s)[2])
                     else:
                         print('("%s",未能判断该数字!)' %fenge(s)[2])
                     an[fenge(s)[0]]=yunsuan(ss,x,y)
                 else:
                     print('(没有%s！)' %fenge(s)[0])
-            elif fenge(s)[0]=='看看':
-                s1=fenge(s)[1]
-                if s1[0]=='“' and s1[len(s1)-1]=='”':
-                    print(s1.strip('“”'))
-                elif an.__contains__(fenge(s)[1])==True:  #看看 （变量名）
-                    if an[fenge(s)[1]]>=0:
-                        print(shuzitohan(an[fenge(s)[1]]))
-                    else:
-                        print('负%s' %shuzitohan(an[fenge(s)[1]]))
-                elif an.__contains__(fenge(s)[1])==False:
-                        print('(没有%s！)' %fenge(s)[1])
+            elif fenge(s)[0]=='看看' and len(fenge(s))==2:
+                if fenge(s)[1]=='':
+                    print('(语句输入格式错误！)')
+                else:
+                    s1=fenge(s)[1]
+                    if s1[0]=='“' and s1[len(s1)-1]=='”':
+                        print(s1.strip('“”'))
+                    elif an.__contains__(s1)==True:  #看看 （变量名）
+                            print(shuzitohan(an[s1]))
+                    elif an.__contains__(s1)==False:
+                            print('(没有%s！)' %s1)
             elif fenge(s)[0]=='如果':   #如果 （判断语句） 则 （操作语句1） 否则 （操作语句2）
                 if len(fenge(s))>=8:
                     ts=re.match('如果 (.*?) 则 (.*?) 否则 (.*)',s,re.S)
@@ -131,55 +166,53 @@ def main():
                     r=ts.group(3)    #（操作语句2）
                     if an.__contains__(fenge(m)[0])==True:
                         x=an[fenge(m)[0]]
-                        if pand(fenge(m)[2])=='True':
+                        if num(fenge(m)[2])=='True' and hantoshuzi(fenge(m)[2])!='False':
                             y=hantoshuzi(fenge(m)[2])
                         else:
-                            print('("%s",未能判断该数字!)' %fenge(m)[2])
+                            str='("'+fenge(n)[2]+'",未能判断该数字!)'
                         if bijiao(fenge(m)[1],x,y)==1:
-                            if fenge(n)[0]=='看看':
-                                s1=fenge(n)[1]
-                                if s1[0]=='“' and s1[len(s1)-1]=='”':
-                                    str=s1.strip('“”')
-                                if an.__contains__(fenge(n)[1])==True: 
-                                    str='(刚刚执行了“'+r+'”操作)'
-                                    if an[fenge(n)[1]]>=0:
-                                        str=fenge(n)[1]+':'+shuzitohan(an[fenge(n)[1]])
-                                    else:
-                                        str=fenge(n)[1]+':'+'负'+shuzitohan(an[fenge(n)[1]])
-                                elif an.__contains__(fenge(n)[1])==False:
-                                    str='(没有'+fenge(n)[1]+'！)'
+                            if fenge(n)[0]=='看看' and len(fenge(n))==2:
+                                if fenge(n)[1]=='':
+                                    str='(语句输入格式错误！)'
+                                else:
+                                    s1=fenge(n)[1]
+                                    if s1[0]=='“' and s1[len(s1)-1]=='”':
+                                        str=s1.strip('“”')
+                                    elif an.__contains__(s1)==True: 
+                                        str=shuzitohan(an[s1])
+                                    elif an.__contains__(s1)==False:
+                                        str='(没有'+s1+'！)'
                             elif n=='无':
                                 str='(没有任何操作)'
                             else:
                                 if an.__contains__(fenge(n)[0])==True:
-                                    if pand(fenge(n)[2])=='True':
+                                    if num(fenge(n)[2])=='True' and hantoshuzi(fenge(n)[2])!='False':
                                         t=hantoshuzi(fenge(n)[2])
                                     else:
-                                        print('("%s",未能判断该数字!)' %fenge(n)[2])
+                                        str='("'+fenge(n)[2]+'",未能判断该数字!)'
                                     x=an[fenge(n)[0]]
                                     an[fenge(n)[0]]=yunsuan(fenge(n)[1],x,t)
                                     str='(刚刚执行了“'+n+'”操作)'
                         else:
-                            if fenge(r)[0]=='看看':
-                                s1=fenge(r)[1]
-                                if s1[0]=='“' and s1[len(s1)-1]=='”':
-                                    str=s1.strip('“”')
-                                elif an.__contains__(fenge(r)[1])==True: 
-                                    str='(刚刚执行了“'+r+'”操作)'
-                                    if an[fenge(r)[1]]>=0:
-                                        str=fenge(r)[1]+':'+shuzitohan(an[fenge(r)[1]])
-                                    else:
-                                        str=fenge(r)[1]+':'+'负'+shuzitohan(an[fenge(r)[1]])
-                                elif an.__contains__(fenge(r)[1])==False:
-                                    str='(没有'+fenge(r)[1]+'！)'
+                            if fenge(r)[0]=='看看'  and len(fenge(r))==2:
+                                if fenge(r)[1]=='':
+                                    str='(语句输入格式错误！)'
+                                else:
+                                    s1=fenge(r)[1]
+                                    if s1[0]=='“' and s1[len(s1)-1]=='”':
+                                        str=s1.strip('“”')
+                                    elif an.__contains__(s1)==True: 
+                                        str=shuzitohan(an[s1])
+                                    elif an.__contains__(s1)==False:
+                                        str='(没有'+s1+'！)'
                             elif r=='无':
                                 str='(没有任何操作)'
                             else:
                                 if an.__contains__(fenge(r)[0])==True:
-                                    if pand(fenge(r)[2])=='True':
+                                    if num(fenge(r)[2])=='True' and hantoshuzi(fenge(r)[2])!='False':
                                         t=hantoshuzi(fenge(r)[2])
                                     else:
-                                        print('("%s",未能判断该数字!)' %fenge(r)[2])
+                                        str='("'+fenge(n)[2]+'",未能判断该数字!)'
                                     x=an[fenge(r)[0]]
                                     an[fenge(r)[0]]=yunsuan(fenge(r)[1],x,t)
                                     str='(刚刚执行了“'+r+'”操作)'
@@ -188,8 +221,6 @@ def main():
                 else:
                     str='(语句输入格式错误！)'
                 print(str)
-
             else:
                 print('(语句输入格式错误！)')
-        
 main()
